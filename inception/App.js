73b398449d249fb0ba803b66f8f83470345730cb
ranjithkumark8 <ranjithkumark8@gmail.com>
2024-07-03 13:25:15 +0530

    completed inception

--- inception/App.js
+++ inception/App.js
@@ -1,12 +1,3 @@
-<div id="parent">
-  <div id="child">
-    <h1>I'm h1 tag</h1>
-  </div>
-  <div>
-    <h2>I'm h2 tag</h2>
-  </div>
-</div>;
-
 // const heading = React.createElement(
 //   "h1",
 //   { id: "heading" },
@@ -14,13 +5,16 @@
 // );
 // console.log(heading);
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I'm h1 tag"),
-  ])
-);
+    React.createElement("h2", {}, 'I"m h2 tag'),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, 'I"m h2 tag'),
+  ]),
+]);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(parent);
